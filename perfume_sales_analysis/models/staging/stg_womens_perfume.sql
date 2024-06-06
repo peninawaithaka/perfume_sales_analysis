30c1af68a609e93de868181d604de13f786c2767
Penina Waithaka <peninawaithaka5@gmail.com>
2024-06-06 19:27:16 +0300

    updated seeds tables

--- perfume_sales_analysis/models/staging/stg_womens_perfume.sql
+++ perfume_sales_analysis/models/staging/stg_womens_perfume.sql
@@ -14,7 +14,7 @@ WITH raw_womens_perfume AS(
         sold as perfumes_sold,
         lastupdated as last_updated,
         itemlocation  
-    FROM PERFUME_SALES.STAGING.WOMENS_PERFUME
+    FROM PERFUME_SALES.ANALYTICS.RAW_WOMENS_PERFUME
 )
 
 SELECT * FROM raw_womens_perfume
